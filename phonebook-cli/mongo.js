cb2bbec0027286e2a7b361fcbefa5734958d82d9
Juuso Pyykkönen <jhpyyk@utu.fi>
2022-04-15 11:02:21 +0300

    Add mongo.js

---
+++ phonebook-cli/mongo.js
@@ -0,0 +1,35 @@
+const args = process.argv.slice(2);
+
+const mongoose = require("mongoose");
+
+const url =
+  "mongodb+srv://jhpyyk:fx5wiyR6nyyGORV6@web-mob-phonebook.7mhmx.mongodb.net/web-mob-phonebook?retryWrites=true&w=majority";
+
+mongoose.connect(url);
+
+const Person = mongoose.model("Person", {
+  name: String,
+  number: String,
+});
+
+if (args.length === 0) {
+  Person.find({}).then((result) => {
+    console.log("puhelinluettelo:");
+    result.forEach((person) => {
+      console.log(person.name + " " + person.number);
+    });
+    mongoose.connection.close();
+  });
+} else if (args.length === 2) {
+  const person = new Person({
+    name: args[0],
+    number: args[1],
+  });
+
+  person.save().then((result) => {
+    console.log("person saved!");
+    mongoose.connection.close();
+  });
+} else {
+  mongoose.connection.close();
+}
